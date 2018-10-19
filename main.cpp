b634cd8a472df949aff265e9b6ddc14bc07c0e0a
TyRoXx <tyroxxxx@gmail.com>
2018-10-19 21:56:49 +0200

    variable assignment

--- main.cpp
+++ main.cpp
@@ -1,67 +1,81 @@
 #include <fstream>
 #include <iostream>
 #include <algorithm>
+#include <map>
 
 namespace oho {
-std::string next_token(char const *&begin, char const *const end) {
-  while ((begin != end) && std::isspace(*begin)) {
-    ++begin;
-  }
-  if (begin == end) {
-    return "";
-  }
-  if (std::isalnum(*begin)) {
-    char const *const begin_of_id = begin;
-    ++begin;
-    while (begin != end && std::isalnum(*begin)) {
-      ++begin;
-    }
-    return std::string(begin_of_id, begin);
-  }
-  if (*begin == '"') {
-    char const *const begin_of_string = begin;
-    ++begin;
-    char const *const end_of_string = std::find(begin, end, '"');
-    begin = end_of_string;
-    if (begin != end) {
-      ++begin;
-    }
-    return std::string(begin_of_string, end_of_string);
-  }
-  ++begin;
-  return std::string(begin - 1, begin);
-}
+struct interpreter {
+  std::map<std::string, std::string> variables;
 
-std::string evaluate_expression(char const *&begin, char const *const end) {
-  std::string const token = next_token(begin, end);
-  if (token.empty()) {
+  std::string next_token(char const *&begin, char const *const end) {
+    while ((begin != end) && std::isspace(*begin)) {
+      ++begin;
+    }
+    if (begin == end) {
+      return "";
+    }
+    if (std::isalnum(*begin) || (*begin == '~')) {
+      char const *const begin_of_id = begin;
+      ++begin;
+      while (begin != end && (std::isalnum(*begin) || (*begin == '~'))) {
+        ++begin;
+      }
+      return std::string(begin_of_id, begin);
+    }
+    if (*begin == '"') {
+      char const *const begin_of_string = begin;
+      ++begin;
+      char const *const end_of_string = std::find(begin, end, '"');
+      begin = end_of_string;
+      if (begin != end) {
+        ++begin;
+      }
+      return std::string(begin_of_string, end_of_string);
+    }
+    ++begin;
+    return std::string(begin - 1, begin);
+  }
+
+  std::string evaluate_expression(char const *&begin, char const *const end) {
+    std::string const token = next_token(begin, end);
+    if (token.empty()) {
+      return token;
+    }
+    if (token[0] == '"') {
+      return token.substr(1, token.size() - 1);
+    }
+    auto found = variables.find(token);
+    if (found != variables.end()) {
+      return found->second;
+    }
     return token;
   }
-  if (token[0] == '"') {
-    return token.substr(1, token.size() - 1);
-  }
-  return token;
-}
 
-void run(char const *begin, char const *const end) {
-  for (;;) {
-    std::string const token = next_token(begin, end);
+  void run(char const *begin, char const *const end) {
+    for (;;) {
+      std::string const token = next_token(begin, end);
 
-    if (token == "") {
-      break;
-    }
-    if (token == "print") {
-      std::string message = evaluate_expression(begin, end);
-      std::cout << message << std::endl;
-    } else if (token == "if") {
-      std::string condition = evaluate_expression(begin, end);
-      std::string colon = next_token(begin, end);
-      if (condition != "true") {
-        begin = std::find(begin, end, '\n');
+      if (token == "") {
+        break;
+      }
+      if (token == "print") {
+        std::string message = evaluate_expression(begin, end);
+        std::cout << message << std::endl;
+      } else if (token == "if") {
+        std::string condition = evaluate_expression(begin, end);
+        std::string colon = next_token(begin, end);
+        if (condition != "true") {
+          begin = std::find(begin, end, '\n');
+        }
+      } else {
+        std::string const next = next_token(begin, end);
+        if (next == "=") {
+          variables[token] = evaluate_expression(begin, end);
+        }
       }
     }
   }
-}
+};
 }
 
 int main(int argc, char **argv) {
@@ -77,5 +91,6 @@ int main(int argc, char **argv) {
   if (!file) {
     return 1;
   }
-  oho::run(source.data(), source.data() + source.size());
+  oho::interpreter i;
+  i.run(source.data(), source.data() + source.size());
 }
